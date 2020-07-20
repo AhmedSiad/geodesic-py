85aa070020a5fb244170f5d7f069a589796d5976
AhmedSiad <63379888+AhmedSiad@users.noreply.github.com>
2020-07-20 15:19:23 -0600

    Improved Readability
    Added comments to make it easier to read through the code

--- graph.py
+++ graph.py
@@ -3,18 +3,19 @@ def generateNSizedGraph(n):
         # do something special for board sizes 2 and 1
         pass
 
-    graph = [[] for i in range(getSmallestCellNumber(n))]
+    graph = [[] for i in range(getSmallestCellNumber(n + 1))]
 
     graph[0] = [1, 2, 3, 4, 8]
     graph[1] = [0, 2, 4, 5, 6]
     graph[2] = [0, 1, 6, 7, 8]
 
+    # Start at depth level 3 because depth level 1 and 2 are trivial
     depth = 3
     while (depth <= n):
-        # Original Cells:
-        smallest = getSmallestCellNumber(depth - 1)
-        biggest = getSmallestCellNumber(depth)
+        smallest = getSmallestCellNumber(depth)
+        biggest = getSmallestCellNumber(depth + 1)
         for i in range(smallest, biggest):
+            #Neighbors within depth level
             if i != biggest - 1:
                 graph[i].append(i + 1)
             else:
@@ -25,24 +26,38 @@ def generateNSizedGraph(n):
             else:
                 graph[i].append(biggest - 1)
 
-            if isCorner(i, depth - 1) and depth != n:
-                cornerNum = cornerNumber(i, depth - 1)
-                nextCorner = getCorner(cornerNum, depth)
-                graph[i].append(nextCorner)
-                graph[i].append(nextCorner + 1)
-                if nextCorner == biggest:
-                    graph[i].append(getSmallestCellNumber(depth + 1) - 1)
+            #Neighbors between current depth level and the depth level above
+            #Ignore depth level when it is equal to the max size of the board, because it has no depth level above it
+            if isCorner(i, depth) and depth != n:
+                #Corner pieces always have 3 neighbors above them
+                cornerNum = getCornerPlace(i, depth)
+                nextCornerUp = getCorner(cornerNum, depth + 1)
+                graph[i].append(nextCornerUp)
+                graph[i].append(nextCornerUp + 1) 
+                if nextCornerUp == biggest:
+                    #This is the center corner 
+                    graph[i].append(getSmallestCellNumber(depth + 2) - 1)
                 else:
-                    graph[i].append(nextCorner - 1)
+                    graph[i].append(nextCornerUp - 1)
             elif depth != n:
-                c = cornerNumber(lastCorner(i, depth - 1), depth - 1)
+                #Regular edge pieces
+                c = getCornerPlace(lastCorner(i, depth), depth)
                 multiple = (depth - 1) * 3
                 graph[i].append(i + multiple + c)
                 graph[i].append(i + multiple + c + 1)
 
         depth += 1
 
-    graph = pairUpGraph(graph)
+    #Add 3 outer edge nodes
+    for i in range(3):
+        corner = getCorner(i, n)
+        graph.append([x for x in range(corner, corner + n)])
+        if i == 2: 
+            #Remove extra last edge, and add the correct corner from the level below
+            del graph[-1][-1]
+            graph[-1].append(getCorner(0, n))
+
+    graph = pairUpGraph(graph) #Pair up any remaining cells where only 1 of them is counted as a neighbor
     return graph
 
 def pairUpGraph(graph):
@@ -56,33 +71,32 @@ def pairUpGraph(graph):
     return g
 
 def getSmallestCellNumber(n):
-    return int(3 * (n - 1) * n/2)
+    return int(3 * (n - 1) * (n - 2)/2)
 
 
 def isCorner(x, depth):
     smallestCell = getSmallestCellNumber(depth)
     if x == smallestCell:
         return True
-    if x == smallestCell + depth:
+    if x == smallestCell + depth - 1:
         return True
-    if x == smallestCell + depth * 2:
+    if x == smallestCell + depth * 2 - 2:
         return True
     return False
 
 
-def cornerNumber(x, depth):
+def getCornerPlace(x, depth):
     smallestCell = getSmallestCellNumber(depth)
     if x == smallestCell:
         return 0
-    if x == smallestCell + depth:
+    if x == smallestCell + depth - 1:
         return 1
-    if x == smallestCell + depth * 2:
+    if x == smallestCell + depth * 2 - 2:
         return 2
-
 
 def getCorner(num, depth):
     smallestCell = getSmallestCellNumber(depth)
-    return smallestCell + depth * num
+    return smallestCell + (depth - 1) * num
 
 def lastCorner(x, depth):
     first = getCorner(0, depth)
